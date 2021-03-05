88b03b96d8c3682881e64894c4f69732ab35e4cd
vyshnav-k <vyshnav@nirandas.com>
2021-03-05 13:40:52 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
-import './App.css';
-import Input from './components/input/Input'
+import "./App.css";
+import Input from "./components/input/Input";
+import Axios from "axios";
+import React, { useState } from "react";
 
 function App() {
+  const [country, setCountry] = useState("");
+  Axios.get("https://freegeoip.app/json/").then((response) => {
+    console.log(response.data.country_name);
+    if (response.data.country_name == "India") {
+      setCountry(response.data.country_name);
+    }
+  });
+
   return (
-    <div className="App">
-      <Input/>
+    <div>
+      {country ? (
+        <Input />
+      ) : (
+        <div>
+          <h3>You are outside of india </h3>
+          <h3>access denied!!</h3>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,33 +1,42 @@
-import React, {useState} from "react";
-import {useDispatch} from 'react-redux'
-import ageCalcActions from '../redux/actions/ageCalcActions'
-import '../input/input.css'
-import Agecalc from '../Agecalc'
-
-
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import ageCalcActions from "../redux/actions/ageCalcActions";
+import "../input/input.css";
+import Agecalc from "../Agecalc";
 
 function Input() {
-  const dispatch=  useDispatch();
-  const [dob,setDob]= useState('')
-  const calculateAge=()=>{
-    dispatch (ageCalcActions.calculateAge({
-      dob,
-    }))
-    
-  }
+  const dispatch = useDispatch();
+  const [dob, setDob] = useState("");
+  const calculateAge = () => {
+    dispatch(
+      ageCalcActions.calculateAge({
+        dob,
+      })
+    );
+  };
   return (
     <div className="inputOuterContainer">
-  <h3> <b><i>Enter your date of birth</i></b></h3>
+      <h3>
+        {" "}
+        <b>
+          <i>Enter your date of birth</i>
+        </b>
+      </h3>
       <div className="inputInnerContainer">
-        <input className="input" type="date" name="date"   onChange={(e) => {
+        <input
+          className="input"
+          type="date"
+          name="date"
+          onChange={(e) => {
             setDob(e.target.value);
-          }} />
+          }}
+        />
 
-        
-        <button className="calc-btn" type="submit" onClick={calculateAge}>Calculate age</button>
+        <button className="calc-btn" type="submit" onClick={calculateAge}>
+          Calculate age
+        </button>
       </div>
-      <Agecalc/>
+      <Agecalc />
     </div>
   );
 }
